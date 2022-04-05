708d5bc9445af0cf921575daca94d838d310ac95
Schubert Anselme <sanselme@mirantis.com>
2022-04-05 18:19:06 -0400

    fix: fixing initial deployment

--- tools/services/010-vault.sh
+++ tools/services/010-vault.sh
@@ -11,7 +11,6 @@ helm upgrade -i vault vault \
   --namespace vault \
   --create-namespace \
   --reuse-values \
-  --set global.tlsDisable=false \
   --set injector.metrics.enabled=true \
   --set server.ingress.enabled=true \
   --set server.ingress.hosts[0].host=vault.${OSH_FQDN} \
@@ -19,17 +18,6 @@ helm upgrade -i vault vault \
   --set server.ingress.tls[0].secretName=vault-tls \
   --set server.ingress.tls[0].hosts[0]=vault.${OSH_FQDN} \
   --set server.auditStorage.enabled=true
-
-# TODO: fix - config
-#   --set server.standalone.config="ui = true
-# listener 'tcp' {
-#   tls_disable = 0
-# address = '[::]:8200'
-# cluster_address = '[::]:8201'
-# }
-# storage 'file' {
-#   path = '/vault/data'
-# }"
 
 # add certificate
 envsubst < manifests/vault/certificate.yaml | kubectl apply -f -
